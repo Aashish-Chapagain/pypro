15d3c81d79796604db04b234145f3ef37c132bda
Aashish-Chapagain <lalchapagain2014161@gmail.com>
2025-03-13 17:53:31 +0545

    learning

--- Bankmgmt.py
+++ Bankmgmt.py
@@ -9,31 +9,58 @@ class Bank : # defining the class Bank
         self.balance = balance 
         Bank._customer += 1 # _costumer is increamented every time the constructor is called 
     
+    @classmethod
+    def create_object(cls):
+        name = input("enter name: ")
+        account_number = int(input("enter the account number: "))
+        balance = int(input("enter the deposit amount: "))
+        return cls(name, account_number, balance)
+
+
 
     def display(self):  # method to display object 
         print(f"""name: {self.name}
 account number: {self.account_number}
 balance: $ {self.balance} \n""")
-        
+
+customers = []
+
+while True : 
+    user_condition = input("enter y to inut data and n to exit: ").strip().lower()
+    if user_condition == "y":
+      customer = Bank.create_object()
+      customers.append(customer)
+
+    elif(user_condition == "n"):
+        break
+    else:
+        print("please enter the correct command: ")
+
+
+for customer in customers : 
+    print(f"customer{Bank._customer}")
+    customer.display()
 
 
 
-user1 = Bank("sponge bob",103299233022,40000 )    # creating object 
-user2 = Bank("patrick", 103872873890, 55000)  # creating object 
-user3 = Bank("squidward", 103879379390, 65000)  # creating object 
-user4 = Bank("mr carbs", 103873422340, 650000)  # creating object 
 
-user1.display()   # using display method to print the object into the console 
-user2.display() # using display method to print the object into the console 
-user3.display() # using display method to print the object into the console 
-user4.display() # using display method to print the object into the console 
+
+    
+
+
+
+# user1 = Bank("sponge bob",103299233022,40000 )    # creating object 
+# user2 = Bank("patrick", 103872873890, 55000)  # creating object 
+# user3 = Bank("squidward", 103879379390, 65000)  # creating object 
+# user4 = Bank("mr carbs", 103873422340, 650000)  # creating object 
+
+# user1.display()   # using display method to print the object into the console 
+# user2.display() # using display method to print the object into the console 
+# user3.display() # using display method to print the object into the console 
+# user4.display() # using display method to print the object into the console 
 
 
 print(f"The number of customers is: ", Bank._customer)  # printing the calss variable to the console 
 
 
-print(user1.name)  
-print(user2.name)  
-print(user3.name)  
-print(user4.name)  
 
