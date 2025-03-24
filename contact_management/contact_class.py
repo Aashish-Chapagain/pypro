ea7c41dcf259914ec03fd5489a97af4a4fe5faa8
Aashish-Chapagain <lalchapagain2014161@gmail.com>
2025-03-24 16:37:28 +0545

    small fixing

--- contact_management/contact_class.py
+++ contact_management/contact_class.py
@@ -13,6 +13,7 @@ class Contacts:
                     self.contacts = json.load(file)
                 except json.JSONDecodeError:
                     self.contacts = {}
+                    
 
     def save_contact(self, name, number, email, address):
         self.name = name
@@ -20,36 +21,38 @@ class Contacts:
         self.email = email
         self.address = address
 
-        self.contacts[name] = {"number": number, "email": email, "address": address}
-
+      
     def save_to_json(self):
         number_list = []
         for value in self.contacts.values():
             number_list.append(value["number"])
-        if not self.number in number_list:
-            with open(self.jsonfile, "w") as file:
-                json.dump(self.contacts, file, indent=3)
+        print(number_list, self.number)
+        if not self.number  in number_list:
+             self.contacts[self.name] = {"number": self.number, "email":self.email, "address":self.address}
 
+             with open(self.jsonfile, "w") as file:
+                json.dump(self.contacts, file, indent=3)
+           
         else:
             print("Number already exists! ")
 
-    def delete_from_json(self, name):
+
+    def delete_from_json(self,name):
         if name in self.contacts:
             del self.contacts[name]
             self.save_to_json()
 
     def search_contact(self, name):
-        list_of_name = []
+        is_avail  = True 
         for key, values in self.contacts.items():
-            list_of_name.append(key)
             if name != key :
+                 
                  continue 
             else : 
                  print(key,values)
-        if name not in list_of_name: 
+                 is_avail = False
+                 
+        if  is_avail:
             print("No mathches found!")
                  
-            
-
-            
-            
+         
--- dictionary.py
+++ dictionary.py
@@ -1,22 +1,60 @@
-
 import json
+# import os
 
-# def check_for_dublicate():
+# # def check_for_dublicate():
+# #     number = int(input())
+# #     number_list = []
+# #     with open("contacts.json", "r") as file :
+# #      temp = json.load(file)
+# #      for value in temp.values():
+# #        number_list = value["number"].append(number_list)
+# #        if number in number_list():
+# #         print("Number already exists! Do you want to continue y/n: ")
+
+# # check_for_dublicate()
+
+
+# # a = input("enter")
+# # with open("contacts.json", "r") as file :
+# #     contacts = json.load(file)
+# #     for key, values in contacts.items():
+# #         if a== key:
+# #             print(key,values)
+
+
+# def save_to_json():
+#     contacts = {}
 #     number = int(input())
+#     if os.path.exists("contacts.json"):
+#         with open("contacts.json", "r") as file:
+#             try:
+#                 contacts = json.load(file)
+#             except json.JSONDecodeError:
+#                 contacts = {}
+#                 contacts = json.load(file)
+              
 #     number_list = []
-#     with open("contacts.json", "r") as file : 
-#      temp = json.load(file)
-#      for value in temp.values():
-#        number_list = value["number"].append(number_list) 
-#        if number in number_list():
-#         print("Number already exists! Do you want to continue y/n: ")
-    
-# check_for_dublicate()
+#     for value in contacts.values():
+#         number_list.append(value["number"])
+#     if not number in number_list:
+#         with open("contacts.json", "w") as file:
+#             json.dump(number, file, indent=3)
+#     else:
+#         print("already exists")
 
 
-a = input("enter")
+# save_to_json()
+
+
+number_list= []
+n = int(input("Enter number: "))
+
 with open("contacts.json", "r") as file : 
     contacts = json.load(file)
-    for key, values in contacts.items():
-        if a== key:
-            print(key,values)
+    for value in contacts.values():
+        number_list.append(value["number"])
+    
+    if n not in number_list : 
+         print(n)
+    else : 
+           print("already exists")
