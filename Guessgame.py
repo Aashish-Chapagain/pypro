9e52a8ed15da2dae25ed7987700c439f6b6f288a
Beeplap <152266714+Beeplap@users.noreply.github.com>
2025-04-09 22:56:07 +0545

    Update Guessgame.py

--- Guessgame.py
+++ Guessgame.py
@@ -3,21 +3,19 @@ import math
 lower = int(input("Enter lower bound: "))
 upper = int(input("Enter upper bound: "))
 x = random.randint(lower, upper)
-chances = round(math.log(upper-lower + 1, 2))
-print("\n\t Total chances you get is ", chances )
+chances = round(math.log(upper - lower + 1, 2))
+print(f"\n\tYou have {chances} chances to guess the number!\n")
 count = 0
 while count < chances:
-    count += 1 
-    a = int(input(" \n Enter your guess: "))
-    if a == x:
-        print("correct ")
-     
-    elif a<x:
-        print("not correct too low ")
+    count += 1
+    guess = int(input(f"Guess {count}: Enter your number: "))
+    if guess == x:
+        print("\n🎉 Correct! You guessed the number!")
+        break
+    elif guess < x:
+        print("Too low!")
     else:
-        print("not correct too high")
-if count <= chances and x != a  :
-    print(f"\n \t The random number is {x} ")
-    print("\n \t Opps! You could not guess it ")
-     
-
+        print("Too high!")
+if guess != x:
+    print(f"\n😢 Oops! You couldn't guess the number.")
+    print(f"The correct number was: {x}")
